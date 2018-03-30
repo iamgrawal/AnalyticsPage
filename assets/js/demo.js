fc82b023d86095b8ee466e8539f4de7975b9c23d
Gaurav Rawal <gaurav.rawal66@gmail.com>
2018-03-30 23:46:58 +0530

    Pie Chart updated

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,51 +1,6 @@
 type = ['', 'info', 'success', 'warning', 'danger'];
 
 demo = {
-  initPickColor: function() {
-    $('.pick-class-label').click(function() {
-      var new_class = $(this).attr('new-class');
-      var old_class = $('#display-buttons').attr('data-class');
-      var display_div = $('#display-buttons');
-      if (display_div.length) {
-        var display_buttons = display_div.find('.btn');
-        display_buttons.removeClass(old_class);
-        display_buttons.addClass(new_class);
-        display_div.attr('data-class', new_class);
-      }
-    });
-  },
-
-  initDocumentationCharts: function() {
-    /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
-
-    dataDailySalesChart = {
-      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-      series: [[12, 17, 7, 17, 23, 18, 38]]
-    };
-
-    optionsDailySalesChart = {
-      lineSmooth: Chartist.Interpolation.cardinal({
-        tension: 0
-      }),
-      low: 0,
-      high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-      chartPadding: {
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0
-      }
-    };
-
-    var dailySalesChart = new Chartist.Line(
-      '#dailySalesChart',
-      dataDailySalesChart,
-      optionsDailySalesChart
-    );
-
-    md.startAnimationForLineChart(dailySalesChart);
-  },
-
   initDashboardPageCharts: function() {
     /* ----------==========     Daily Sales Chart initialization    ==========---------- */
 
@@ -89,7 +44,11 @@ demo = {
       dataCompletedTasksChart,
       {
         labelInterpolationFnc: function(value) {
-          return Math.round(value / data.series.reduce(sum) * 100) + '%';
+          return (
+            Math.round(
+              value / dataCompletedTasksChart.series.reduce(sum) * 100
+            ) + '%'
+          );
         }
       }
     );
